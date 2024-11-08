445890f372122de79816402aac0581669740ad13
Harry.Cleveland <109498948+hccleveland@users.noreply.github.com>
2024-11-08 22:06:27 +0900

    feat: add getObjects with param options

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -17,3 +17,30 @@ export async function getDepartments(): Promise<Departments[]> {
     }
     return resData.departments;
 }
+
+interface ResponseObjects {
+    total: number;
+    objectIDs: number[];
+}
+
+export async function getObjects(paramObj?: {
+    metadataDate?: string;
+    departmentId?: string;
+}): Promise<number[]> {
+    let urlString =
+        "https://collectionapi.metmuseum.org/public/collection/v1/objects";
+    if (paramObj !== undefined) {
+        if (paramObj.metadataDate !== undefined) {
+            urlString = `${urlString}?metadataDate=${paramObj.metadataDate}`;
+        }
+        if (paramObj.departmentId !== undefined) {
+            urlString = `${urlString}?departmentIds=${paramObj.departmentId}`;
+        }
+    }
+    const response = await fetch(urlString);
+    const resData = (await response.json()) as ResponseObjects;
+    if (!response.ok) {
+        throw new Error("Failed to fetch objects data.");
+    }
+    return resData.objectIDs;
+}
