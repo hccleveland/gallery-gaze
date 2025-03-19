c0b01f54efd40f575e78e8a3072464b5664f0261
Harry.Cleveland <109498948+hccleveland@users.noreply.github.com>
2025-03-19 09:20:23 +0900

    feat: add moveRandomizedIndex dispatch actions

--- frontend/src/store/collectionSlice.ts
+++ frontend/src/store/collectionSlice.ts
@@ -17,6 +17,21 @@ const initialState: CollectionState = {
     error: null,
 };
 
+const randomizeMoveIndex = (arrayTotal: number) => {
+    let pickedNumber = 1;
+    if (arrayTotal < 1000) {
+        const upperRange = Math.round(arrayTotal * 0.025);
+        pickedNumber = Math.floor(Math.random() * upperRange) + 1;
+    } else if (arrayTotal < 10000) {
+        const upperRange = Math.round(arrayTotal * 0.01);
+        pickedNumber = Math.floor(Math.random() * upperRange) + 1;
+    } else {
+        const upperRange = Math.round(arrayTotal * 0.005);
+        pickedNumber = Math.floor(Math.random() * upperRange) + 1;
+    }
+    return pickedNumber;
+};
+
 export const fetchCollectionFromDepartmentID = createAsyncThunk(
     "collection/fetchCollection",
     async (paramObj: { metadataDate?: string; departmentId?: string }) => {
@@ -45,6 +60,31 @@ export const collectionSlice = createSlice({
                 state.currentIndex === 0
                     ? state.objectIDs.length - 1
                     : state.currentIndex - 1;
+        },
+        moveNextRandomizedIndex(state) {
+            const randomizedStep = randomizeMoveIndex(
+                state.objectIDs.length - 1
+            );
+            const newPosition = state.currentIndex + randomizedStep;
+            if (newPosition <= state.objectIDs.length - 1) {
+                state.currentIndex = newPosition;
+            } else {
+                const correctedIndex =
+                    newPosition - (state.objectIDs.length - 1);
+                state.currentIndex = correctedIndex;
+            }
+        },
+        movePrevRandomizedIndex(state) {
+            const randomizedStep = randomizeMoveIndex(
+                state.objectIDs.length - 1
+            );
+            const newPosition = state.currentIndex - randomizedStep;
+            if (newPosition >= 0) {
+                state.currentIndex = newPosition;
+            } else {
+                const correctedStep = randomizedStep - state.currentIndex;
+                state.currentIndex = state.objectIDs.length - 1 - correctedStep;
+            }
         },
     },
     extraReducers: (builder) => {
